579865091d859da4853b23ad50bf8bd9baf46a07
Mahadev <devraj40u@gmail.com>
2025-05-13 16:46:27 +0530

    Added

--- migrations/20250304074740-create-user-role.js
+++ migrations/20250304074740-create-user-role.js
@@ -7,7 +7,7 @@ module.exports = {
       userId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: { model: "Users", key: "id" },
+        references: { model: "users", key: "id" },
         onDelete: "CASCADE",
       },
       roleId: {
