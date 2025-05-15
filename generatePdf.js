456ad15ae02327df934e35c66ba8cea5045e9d49
Mahadev <devraj40u@gmail.com>
2025-05-15 18:47:46 +0530

    Pdf Added

--- generatePdf.js
+++ generatePdf.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
+const puppeteer = require('puppeteer');
 const path = require('path');
 const ejs = require('ejs');
-const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 const generatePdf = async (data) => {
   try {
@@ -11,22 +11,24 @@ const generatePdf = async (data) => {
     // Render the HTML from the EJS template and data
     const html = await ejs.renderFile(templatePath, data);
     
-    // Launch Puppeteer browser instance
-    const browser = await puppeteer.launch();
+    // Launch Puppeteer browser instance with `--no-sandbox` if required
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     
     // Create a new page and set the rendered HTML content
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
     
-    // Generate the PDF from the page content
+   
     const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
     
-    // Close the browser instance
+    
     await browser.close();
     
-    return pdfBuffer; // Return the PDF buffer
+    return pdfBuffer; 
   } catch (error) {
-    console.error('❌ Error generating PDF:', error);
+    console.error('Error generating PDF:', error);
     throw error;
   }
 };
